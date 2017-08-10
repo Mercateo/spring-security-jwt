4c72643121f2e936a32e690fd91b7bac74932ef1
Andreas Würl <andi@tryb.de>
2017-08-10 10:33:24 +0200

    remove unused code

--- src/main/java/com/mercateo/spring/security/jwt/exception/AmbiguousClaimException.java
+++
@@ -1,9 +0,0 @@
-package com.mercateo.spring.security.jwt.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class AmbiguousClaimException extends AuthenticationException {
-    public AmbiguousClaimException(String msg) {
-        super(msg);
-    }
-}
